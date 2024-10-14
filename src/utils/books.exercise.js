d2cb3234a96e2ae37a863a4b85f257d7cd9adf44
Lucas Paiva <lcspaiva87@gmail.com>
2024-10-14 19:28:54 -0300

    Refactor components to utilize custom hooks for data fetching and error handling with react-query

--- src/utils/books.exercise.js
+++ src/utils/books.exercise.js
@@ -1,46 +1,56 @@
 import bookPlaceholderSvg from 'assets/book-placeholder.svg'
-import { queryCache, useQuery } from 'react-query'
-import { client } from './api-client'
+import {queryCache, useQuery} from 'react-query'
+import {client} from './api-client'
 const loadingBook = {
-    title: 'Loading...',
-    author: 'loading...',
-    coverImageUrl: bookPlaceholderSvg,
-    publisher: 'Loading Publishing',
-    synopsis: 'Loading...',
-    loadingBook: true,
+  title: 'Loading...',
+  author: 'loading...',
+  coverImageUrl: bookPlaceholderSvg,
+  publisher: 'Loading Publishing',
+  synopsis: 'Loading...',
+  loadingBook: true,
 }
 const loadingBooks = Array.from({length: 10}, (v, index) => ({
-    id: `loading-book-${index}`,
-    ...loadingBook,
+  id: `loading-book-${index}`,
+  ...loadingBook,
 }))
 
-const getBookSearchConfig =(query,user) => ({
-    queryKey: ['bookSearch', {query}],
-    queryFn: () =>
-      client(`books?query=${encodeURIComponent(query)}`, {
-        token: user.token,
-      }).then(data => data.books),
+const getBookSearchConfig = (query, user) => ({
+  queryKey: ['bookSearch', {query}],
+  queryFn: () =>
+    client(`books?query=${encodeURIComponent(query)}`, {
+      token: user.token,
+    }).then(data => data.books),
+  config: {
+    onSucess(books) {
+      books.forEach(book => setQueryDataForBook(book))
+    },
+  },
 })
 
-function useBookSearch(query,user){
-    console.log(query)
-    const result = useQuery(getBookSearchConfig(query,user))
-    console.log(result)
-    return {...result, books: result.data ?? loadingBooks}
+function useBookSearch(query, user) {
+  console.log(query)
+  const result = useQuery(getBookSearchConfig(query, user))
+  console.log(result)
+  return {...result, books: result.data ?? loadingBooks}
 }
-function useBook(bookId,user){
-    const { data}= useQuery({
-        queryKey: ['book', {bookId}],
-        queryFn: () =>
-          client(`books/${bookId}`, {
-            token: user.token,
-          }).then(data => data.book),
-      })
-      return data ?? loadingBook
-}
-function refetchBookSearchQuery(user){
-    queryCache.removeQueries('bookSearch')
-    queryCache.prefetchQuery(getBookSearchConfig('',user))
-}
-export { refetchBookSearchQuery, useBook, useBookSearch }
 
+function useBook(bookId, user) {
+  const {data} = useQuery({
+    queryKey: ['book', {bookId}],
+    queryFn: () =>
+      client(`books/${bookId}`, {
+        token: user.token,
+      }).then(data => data.book),
+  })
+  return data ?? loadingBook
+}
+
+function refetchBookSearchQuery(user) {
+  queryCache.removeQueries('bookSearch')
+  queryCache.prefetchQuery(getBookSearchConfig('', user))
+}
+
+function setQueryDataForBook(book) {
+  queryCache.setQueryData(['book', {bookId: book.id}], book)
+}
+export {refetchBookSearchQuery, setQueryDataForBook, useBook, useBookSearch}
--- src/utils/list-items.exercise.js
+++ src/utils/list-items.exercise.js
@@ -1,62 +1,82 @@
-const { useQuery, useMutation, queryCache } = require("react-query/dist/react-query.development");
-const { client } = require("./api-client");
+import {setQueryDataForBook} from './books.exercise'
+
+const {
+  useQuery,
+  useMutation,
+  queryCache,
+} = require('react-query/dist/react-query.development')
+const {client} = require('./api-client')
 function useListItems(user) {
-  const {data: listItem} = useQuery({  
+  const {data: listItem} = useQuery({
     queryKey: 'list-items',
-    queryFn: () => client('list-items', {token: user.token}).then(data => data.listItems),
+    queryFn: () =>
+      client('list-items', {token: user.token}).then(data => data.listItems),
+    config: {
+      onSucess(listItems) {
+        listItems.forEach(book => setQueryDataForBook(book, user.token))
+      },
+    },
+  })
 
-    
-   })
-   return listItem || []
-} 
+  return listItem || []
+}
 
+const defaultMutationOptions = {
+  onSettled: () => queryCache.invalidateQueries('list-items'),
+  throwOnError: true,
+}
+function useRemoveListItem(user, options) {
+  return useMutation(
+    ({listItemId}) => {
+      return client(`list-items/${listItemId}`, {
+        method: 'DELETE',
+        token: user.token,
+      })
+    },
+    {
+      ...defaultMutationOptions,
+      ...options,
+    },
+  )
+}
+function useListItem(user, bookId) {
+  const listItems = useListItems(user)
+  return listItems.find(li => li.bookId === bookId) || null
+}
 
-const defaultMutationOptions= {
-    onSettled: () => queryCache.invalidateQueries('list-items'),
-    throwOnError: true,
+function useUpdateItems(user, options) {
+  return useMutation(
+    updates => {
+      return client('list-items', {
+        method: 'PUT',
+        data: updates,
+        token: user.token,
+      })
+    },
+    {
+      ...defaultMutationOptions,
+      ...options,
+    },
+  )
 }
-function useRemoveListItem(user,options) {
-    return useMutation(
-        ({listItemId}) => {
-            return client(`list-items/${listItemId}`, {
-                method: 'DELETE',
-                token: user.token,
-            })
-        },
-        {
-            ...defaultMutationOptions,...options
-        }
-    )
-}
-function useListItem(user,bookId) {
-    const listItems = useListItems(user)
-return listItems.find(li => li.bookId === bookId) || null
-}
-function useUpdateItems(user,options) {
-    return useMutation(
-        updates => {
-            return client('list-items', {
-                method: 'PUT',
-                data: updates,
-                token: user.token,
-            })
-        },
-        {
-            ...defaultMutationOptions,...options
-        }
-    )
-}
-function useCreateListItem(user,options){
-    return useMutation(
-        ({bookId}) =>
-        client('list-items', {
-            data: {bookId},
-            token: user.user.token,
-        }),
-        {
-            ...defaultMutationOptions,...options
-        }
-    )
-}
-export { useCreateListItem, useListItem, useListItems, useRemoveListItem, useUpdateItems };
 
+function useCreateListItem(user, options) {
+  return useMutation(
+    ({bookId}) =>
+      client('list-items', {
+        data: {bookId},
+        token: user.user.token,
+      }),
+    {
+      ...defaultMutationOptions,
+      ...options,
+    },
+  )
+}
+export {
+  useCreateListItem,
+  useListItem,
+  useListItems,
+  useRemoveListItem,
+  useUpdateItems,
+}
